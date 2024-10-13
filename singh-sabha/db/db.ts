63832152fbce5a44dc6e444a7950b7f11726c5ab
Bhavanvir Rai <bhavanvir.r@gmail.com>
2024-10-12 20:24:47 -0700

    Fix issue with ts error

--- singh-sabha/db/db.ts
+++ singh-sabha/db/db.ts
@@ -5,6 +5,12 @@ import postgres from "postgres";
 
 const connectionString = process.env.DATABASE_URL;
 
+if (!connectionString) {
+  throw new Error(
+    "Connection string is undefined. Please set the CONNECTION_STRING environment variable.",
+  );
+}
+
 // Disable prefetch as it is not supported for "Transaction" pool mode
 export const client = postgres(connectionString, { prepare: false });
 export const db = drizzle(client);
