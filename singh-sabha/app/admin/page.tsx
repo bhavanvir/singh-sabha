5689636dadcc40fbf990923c65606e47d849c006
Bhavanvir Rai <bhavanvir.r@gmail.com>
2024-10-13 18:03:46 -0700

    Change login flow

--- singh-sabha/app/admin/page.tsx
+++ singh-sabha/app/admin/page.tsx
@@ -1,46 +1,5 @@
-import { Button } from "@/components/ui/button";
-import {
-  Card,
-  CardContent,
-  CardDescription,
-  CardFooter,
-  CardHeader,
-  CardTitle,
-} from "@/components/ui/card";
-import { Input } from "@/components/ui/input";
-import { Label } from "@/components/ui/label";
-
-function LoginForm() {
-  return (
-    <div className="flex h-screen">
-      <Card className="w-full max-w-sm mx-auto my-auto">
-        <CardHeader>
-          <CardTitle className="text-2xl">Admin login</CardTitle>
-          <CardDescription>
-            Enter your email below to login to your account.
-          </CardDescription>
-        </CardHeader>
-        <CardContent className="grid gap-4">
-          <div className="grid gap-2">
-            <Label htmlFor="email">Email</Label>
-            <Input
-              id="email"
-              type="email"
-              placeholder="me@example.com"
-              required
-            />
-          </div>
-          <div className="grid gap-2">
-            <Label htmlFor="password">Password</Label>
-            <Input id="password" type="password" required />
-          </div>
-        </CardContent>
-        <CardFooter>
-          <Button className="w-full">Sign in</Button>
-        </CardFooter>
-      </Card>
-    </div>
-  );
+function AdminDashboard() {
+  return <h1>Yo</h1>;
 }
 
-export default LoginForm;
+export default AdminDashboard;
