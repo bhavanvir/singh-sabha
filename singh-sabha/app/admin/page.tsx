220d4c59b834eaaadb677ab8fe225fe656e8e4b4
Bhavanvir Rai <bhavanvir.r@gmail.com>
2024-10-16 16:03:52 -0700

    Fetch events and pass to calendar

--- singh-sabha/app/admin/page.tsx
+++ singh-sabha/app/admin/page.tsx
@@ -1,15 +1,18 @@
 import { validateRequest } from "@/lib/auth";
 import LoginForm from "@/components/login";
 import { AdminPanel } from "@/components/admin-panel";
+import { GetAllEvents } from "@/lib/api/events/queries";
 
 export default async function Page() {
   const { user } = await validateRequest();
   if (!user) {
     return <LoginForm />;
   }
+
+  const events = await GetAllEvents();
   return (
     <>
-      <AdminPanel user={user} />
+      <AdminPanel user={user} events={events} />
     </>
   );
 }
