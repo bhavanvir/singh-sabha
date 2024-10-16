a092d93cbae8f2bbd795b2c5cc57dcc66c6b1483
Bhavanvir Rai <bhavanvir.r@gmail.com>
2024-10-16 16:03:29 -0700

    Add function to fetch all events

---
+++ singh-sabha/lib/api/events/queries.ts
@@ -0,0 +1,18 @@
+"use server";
+
+import { cache } from "react";
+import { db } from "@/db/db";
+import { eventTable } from "@/db/schema";
+
+import type { InferSelectModel } from "drizzle-orm";
+
+type Event = InferSelectModel<typeof eventTable>;
+
+export const GetAllEvents = cache(async (): Promise<Event[]> => {
+  try {
+    const events = await db.select().from(eventTable);
+    return events;
+  } catch (err) {
+    throw new Error("Could not fetch events.");
+  }
+});
