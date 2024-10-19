6cd4e3c2364eca871beafdc97fb122e6ffaa7aa0
Bhavanvir Rai <bhavanvir.r@gmail.com>
2024-10-18 22:12:39 -0700

    Change font

--- singh-sabha/app/layout.tsx
+++ singh-sabha/app/layout.tsx
@@ -1,6 +1,12 @@
 import { Toaster } from "sonner";
 import type { Metadata } from "next";
+import { Inter } from "next/font/google";
 import "./globals.css";
+
+const inter = Inter({
+  subsets: ["latin"],
+  display: "swap",
+});
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -13,8 +19,8 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body className="antialiased">{children}</body>
+    <html lang="en" className={inter.className}>
+      <body>{children}</body>
       <Toaster richColors={true} />
     </html>
   );
