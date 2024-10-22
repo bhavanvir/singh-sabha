ee914c9b91eafe08c2e6e0cc569320fce35866dc
Bhavanvir Rai <bhavanvir.r@gmail.com>
2024-10-21 20:27:41 -0700

    Add initial navbar component

---
+++ singh-sabha/components/client-navbar.tsx
@@ -0,0 +1,41 @@
+import Link from "next/link";
+import { Button } from "@/components/ui/button";
+import { Sheet, SheetContent, SheetTrigger } from "@/components/ui/sheet";
+import { Menu, Package2 } from "lucide-react";
+
+export default function NavBar() {
+  return (
+    <header className="z-10 sticky top-0 flex h-16 items-center gap-4 border-b bg-background px-4 md:px-6">
+      <nav className="hidden flex-col gap-6 text-lg font-medium md:flex md:flex-row md:items-center md:gap-5 md:text-sm lg:gap-6">
+        <Link
+          href="#"
+          className="flex items-center gap-2 text-lg font-semibold md:text-base"
+        >
+          <Package2 className="h-6 w-6" />
+          <span className="sr-only">Gurdwara Singh Sabha</span>
+        </Link>
+        <Link href="/calendar">Calendar</Link>
+      </nav>
+      <Sheet>
+        <SheetTrigger asChild>
+          <Button variant="outline" size="icon" className="shrink-0 md:hidden">
+            <Menu className="h-5 w-5" />
+            <span className="sr-only">Toggle navigation menu</span>
+          </Button>
+        </SheetTrigger>
+        <SheetContent side="left">
+          <nav className="grid gap-6 text-lg font-medium">
+            <Link
+              href="/calendar"
+              className="flex items-center gap-2 text-lg font-semibold"
+            >
+              <Package2 className="h-6 w-6" />
+              <span className="sr-only">Gurdwara Singh Sabha</span>
+            </Link>
+            <Link href="#">Calendar</Link>
+          </nav>
+        </SheetContent>
+      </Sheet>
+    </header>
+  );
+}
