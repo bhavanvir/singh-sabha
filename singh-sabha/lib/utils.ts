04514d538fee00fbe83adc471d9ac97a95ce5c30
Bhavanvir Rai <bhavanvir.r@gmail.com>
2024-10-21 18:44:15 -0700

    Update palette

--- singh-sabha/lib/utils.ts
+++ singh-sabha/lib/utils.ts
@@ -16,11 +16,13 @@ export function kebabToTitleCase(
   }
 }
 
-// From https://coolors.co/palettes/trending
+// From https://coolors.co/palette/f94144-f3722c-f8961e-f9844a-f9c74f-90be6d-43aa8b-4d908e-577590-277da1
 export const typeColourMap: Record<string, string> = {
-  "akhand-path": "#cdb4db",
-  wedding: "#ffc8dd",
-  funeral: "#ffafcc",
-  langar: "#bde0fe",
-  "sehaj-path": "#a2d2ff",
+  "akhand-path": "#f94144",
+  "anand-karaj": "#f3722c",
+  funeral: "#f8961e",
+  langar: "#f9844a",
+  "sehaj-path": "#f9c74f",
+  "sukhmani-sahib-path": "#90be6d",
+  "rainsabai-kirtan": "#43aa8b",
 };
