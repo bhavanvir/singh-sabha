5ab751dd6b776f37ed7133c1e7b7f8cbca82ee26
Bhavanvir Rai <bhavanvir.r@gmail.com>
2024-10-21 20:26:54 -0700

    Add GetAllVerifiedEvents endpoint

--- singh-sabha/lib/api/events/queries.ts
+++ singh-sabha/lib/api/events/queries.ts
@@ -3,10 +3,22 @@
 import { cache } from "react";
 import { db } from "@/db/db";
 import { eventTable } from "@/db/schema";
+import { eq } from "drizzle-orm";
 
-import type { Event } from "@/lib/types/event";
+// TODO: Fix type
+export const GetAllVerifiedEvents = cache(async (): Promise<any> => {
+  try {
+    const events = await db
+      .select()
+      .from(eventTable)
+      .where(eq(eventTable.verified, true));
+    return events;
+  } catch (err) {
+    throw new Error(`Could not fetch events: ${err}`);
+  }
+});
 
-export const GetAllEvents = cache(async (): Promise<Event[]> => {
+export const GetAllEvents = cache(async (): Promise<any> => {
   try {
     const events = await db.select().from(eventTable);
     return events;
