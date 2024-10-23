84e92c6135b640c1b494fb31dfa0534e0d10ac92
Bhavanvir Rai <bhavanvir.r@gmail.com>
2024-10-22 22:44:38 -0700

    Add logic to check if unverified events are overlapping with verified events

--- singh-sabha/app/admin/page.tsx
+++ singh-sabha/app/admin/page.tsx
@@ -1,7 +1,10 @@
 import { validateRequest } from "@/lib/auth";
 import LoginForm from "@/components/login";
 import { Dashboard } from "@/components/dashboard";
-import { GetAllEvents } from "@/lib/api/events/queries";
+import { GetAllEvents, GetAllUnverifiedEvents } from "@/lib/api/events/queries";
+import moment from "moment";
+
+import type { Event } from "@/lib/types/event";
 
 export default async function Page() {
   const { user } = await validateRequest();
@@ -10,9 +13,43 @@ export default async function Page() {
   }
 
   const events = await GetAllEvents();
+  const notifications = await GetAllUnverifiedEvents();
+
+  const verifiedEvents = events.filter(
+    (event: Event) => event.verified === true,
+  );
+
+  for (let i = 0; i < notifications.length; i++) {
+    const unverifiedEvent = notifications[i];
+    const startA = moment(unverifiedEvent.start);
+    const endA = moment(unverifiedEvent.end);
+
+    for (let j = 0; j < verifiedEvents.length; j++) {
+      const verifiedEvent = verifiedEvents[j];
+      const startB = moment(verifiedEvent.start);
+      const endB = moment(verifiedEvent.end);
+
+      // Three cases:
+      // 1. The start date of eventA and eventB are the same
+      // 2. The end date of eventB and eventB are the same
+      // 3. The times overlap for eventA and eventB
+
+      if (startA.isSame(startB) || endA.isSame(endB)) {
+        notifications[i].conflict = true;
+      } else if (
+        startA.isBetween(startB, endB, null, "[)") ||
+        startB.isBetween(startA, endA, null, "[)")
+      ) {
+        notifications[i].conflict = true;
+      } else {
+        notifications[i].conflict = false;
+      }
+    }
+  }
+
   return (
     <>
-      <Dashboard user={user} events={events} />
+      <Dashboard user={user} events={events} notifications={notifications} />
     </>
   );
 }
