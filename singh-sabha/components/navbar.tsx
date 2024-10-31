6bf8221be8583ced37ca530878391183858f10ae
Bhavanvir Rai <bhavanvir.r@gmail.com>
2024-10-31 14:11:16 -0700

    Add hukamnama link

--- singh-sabha/components/navbar.tsx
+++ singh-sabha/components/navbar.tsx
@@ -12,6 +12,7 @@ export default function NavBar({ currentLink }: NavBarProps) {
     { href: "/calendar", label: "Calendar" },
     { href: "/about", label: "About" },
     { href: "/contact", label: "Contact" },
+    { href: "/hukamnama", label: "Hukamnama" },
   ];
 
   return (
