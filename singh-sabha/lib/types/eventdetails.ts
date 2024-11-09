f0a7aa9b879c70f4869dace11656d7fdfb6e5c5b
Bhavanvir Rai <bhavanvir.r@gmail.com>
2024-11-08 20:22:28 -0800

    Add EventColours mapping

--- singh-sabha/lib/types/eventdetails.ts
+++ singh-sabha/lib/types/eventdetails.ts
@@ -1,3 +1,8 @@
+export const EventColors = {
+  regular: "#3b82f6",
+  special: "#a855f7",
+} as const;
+
 export type EventDetails = {
   colour: string;
   displayName: string;
@@ -8,80 +13,80 @@ export type EventDetails = {
 export const typeEventMap: Record<string, EventDetails> = {
   // Regular events
   "akhand-path": {
-    colour: "#3b82f6",
+    colour: EventColors.regular,
     displayName: "Akhand Path",
     isRequestable: true,
     isSpecial: false,
   },
   "anand-karaj": {
-    colour: "#3b82f6",
+    colour: EventColors.regular,
     displayName: "Anand Karaj",
     isRequestable: true,
     isSpecial: false,
   },
   funeral: {
-    colour: "#3b82f6",
+    colour: EventColors.regular,
     displayName: "Funeral",
     isRequestable: true,
     isSpecial: false,
   },
   langar: {
-    colour: "#3b82f6",
+    colour: EventColors.regular,
     displayName: "Langar",
     isRequestable: true,
     isSpecial: false,
   },
   "sehaj-path": {
-    colour: "#3b82f6",
+    colour: EventColors.regular,
     displayName: "Sehaj Path",
     isRequestable: true,
     isSpecial: false,
   },
   "sukhmani-sahib-path": {
-    colour: "#3b82f6",
+    colour: EventColors.regular,
     displayName: "Sukhmani Sahib Path",
     isRequestable: true,
     isSpecial: false,
   },
   "rainsabai-kirtan": {
-    colour: "#3b82f6",
+    colour: EventColors.regular,
     displayName: "Rainsabai Kirtan",
     isRequestable: false,
     isSpecial: false,
   },
   gurpurab: {
-    colour: "#3b82f6",
+    colour: EventColors.regular,
     displayName: "Gurpurab",
     isRequestable: false,
     isSpecial: false,
   },
   "shaheedi-diwas": {
-    colour: "#3b82f6",
+    colour: EventColors.regular,
     displayName: "Shaheedi Diwas",
     isRequestable: false,
     isSpecial: false,
   },
   // Special events
   "new-years": {
-    colour: "#a855f7",
+    colour: EventColors.special,
     displayName: "New Years",
     isRequestable: false,
     isSpecial: true,
   },
   baisakhi: {
-    colour: "#a855f7",
+    colour: EventColors.special,
     displayName: "Baisakhi",
     isRequestable: false,
     isSpecial: true,
   },
   "bandi-chhor-divas": {
-    colour: "#a855f7",
+    colour: EventColors.special,
     displayName: "Bandi Chhor Divas",
     isRequestable: false,
     isSpecial: true,
   },
   "nagar-kirtan": {
-    colour: "#a855f7",
+    colour: EventColors.special,
     displayName: "Nagar Kirtan",
     isRequestable: false,
     isSpecial: true,
