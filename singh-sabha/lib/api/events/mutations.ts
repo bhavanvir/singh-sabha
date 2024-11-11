40db2bef9468f0d210bc11bdd98ac08e5ce2217e
Bhavanvir Rai <bhavanvir.r@gmail.com>
2024-11-10 18:09:52 -0800

    Update mutations to add/edit/delete event types

--- singh-sabha/lib/api/events/mutations.ts
+++ singh-sabha/lib/api/events/mutations.ts
@@ -1,12 +1,19 @@
 "use server";
 
 import { db } from "@/db/db";
-import { eventTable, mailTable, otpTable, userTable } from "@/db/schema";
+import {
+  eventTable,
+  eventTypeTable,
+  mailTable,
+  otpTable,
+  userTable,
+} from "@/db/schema";
 import { revalidatePath } from "next/cache";
 import { eq, and } from "drizzle-orm";
+import { hash } from "@node-rs/argon2";
 
 import type { Event } from "@/lib/types/event";
-import { hash } from "@node-rs/argon2";
+import type { EventType } from "@/lib/types/eventtype";
 
 export const CreateEvent = async ({
   newEvent,
@@ -227,5 +234,67 @@ export const RemoveEmail = async ({ id }: { id: string }): Promise<void> => {
     revalidatePath("/admin");
   } catch (err) {
     throw new Error(`Could not update password: ${err}`);
+  }
+};
+
+export const CreateEventType = async ({
+  eventType,
+}: {
+  eventType: EventType;
+}): Promise<void> => {
+  if (!eventType.displayName) {
+    throw new Error("Missing required parameter to create an event type");
+  }
+
+  try {
+    await db.insert(eventTypeTable).values({
+      displayName: eventType.displayName,
+      isSpecial: eventType.isSpecial,
+      isRequestable: eventType.isRequestable,
+    });
+    revalidatePath("/admin");
+  } catch (err) {
+    throw new Error(`Could not create event type: ${err}`);
+  }
+};
+
+export const UpdateEventType = async ({
+  eventType,
+}: {
+  eventType: EventType;
+}): Promise<void> => {
+  if (!eventType.id) {
+    throw new Error("Missing required parameter to create an event type");
+  }
+
+  try {
+    await db
+      .update(eventTypeTable)
+      .set({
+        displayName: eventType.displayName,
+        isSpecial: eventType.isSpecial,
+        isRequestable: eventType.isRequestable,
+      })
+      .where(eq(eventTypeTable.id, eventType.id!));
+    revalidatePath("/admin");
+  } catch (err) {
+    throw new Error(`Could not update event type: ${err}`);
+  }
+};
+
+export const DeleteEventType = async ({
+  id,
+}: {
+  id: string;
+}): Promise<void> => {
+  if (!id) {
+    throw new Error("Missing required parameter to create an event type");
+  }
+
+  try {
+    await db.delete(eventTypeTable).where(eq(eventTypeTable.id, id));
+    revalidatePath("/admin");
+  } catch (err) {
+    throw new Error(`Could not update event type: ${err}`);
   }
 };
