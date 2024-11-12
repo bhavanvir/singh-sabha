db5593eeff522e9d05daeac54e9f3f80de67e38b
Bhavanvir Rai <bhavanvir.r@gmail.com>
2024-11-11 20:22:48 -0800

    Make it so sessionTable deletes entry upon user deletion

--- singh-sabha/db/schema.ts
+++ singh-sabha/db/schema.ts
@@ -26,7 +26,7 @@ export const sessionTable = pgTable("sessions", {
   id: text("id").primaryKey(),
   userId: text("user_id")
     .notNull()
-    .references(() => userTable.id),
+    .references(() => userTable.id, { onDelete: "cascade" }),
   expiresAt: timestamp("expires_at", {
     withTimezone: true,
     mode: "date",
