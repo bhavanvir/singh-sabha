f0668329a7ee91172e05dbf320c7613bb88159ba
Bhavanvir Rai <bhavanvir.r@gmail.com>
2024-11-16 20:19:05 -0800

    Display the announcement on the home page

--- singh-sabha/app/page.tsx
+++ singh-sabha/app/page.tsx
@@ -1,10 +1,25 @@
+import { Alert, AlertDescription, AlertTitle } from "@/components/ui/alert";
+import { Suspense } from "react";
+import { ActiveAnnouncement } from "@/components/announcement";
 import NavBar from "@/components/navbar";
+import { Loader2 } from "lucide-react";
 
 export default function Home() {
   return (
     <>
       <NavBar currentLink="" />
-      <main className="p-4"></main>
+      <main className="p-4">
+        <Suspense
+          fallback={
+            <Alert>
+              <Loader2 className="h-4 w-4 animate-spin" />
+              <AlertTitle>Loading announcement...</AlertTitle>
+            </Alert>
+          }
+        >
+          <ActiveAnnouncement />
+        </Suspense>
+      </main>
     </>
   );
 }
---
+++ singh-sabha/components/announcement.tsx
@@ -0,0 +1,15 @@
+import { Alert, AlertDescription, AlertTitle } from "@/components/ui/alert";
+import { Rss } from "lucide-react";
+import { GetActiveAnnouncement } from "@/lib/api/events/queries";
+
+export async function ActiveAnnouncement() {
+  const [announcement] = await GetActiveAnnouncement();
+
+  return (
+    <Alert>
+      <Rss className="h-4 w-4" />
+      <AlertTitle>{announcement.title}</AlertTitle>
+      <AlertDescription>{announcement.message}</AlertDescription>
+    </Alert>
+  );
+}
