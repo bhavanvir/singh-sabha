c1976db1a7bd6e41cd3dcf14ed0a0478ed77e70d
Bhavanvir Rai <bhavanvir.r@gmail.com>
2024-11-19 22:18:39 -0800

    Migrate away from using lucia

--- singh-sabha/components/actions/login-action.ts
+++ singh-sabha/components/actions/login-action.ts
@@ -3,18 +3,21 @@
 import { db } from "@/db/db";
 import { verify } from "@node-rs/argon2";
 import { userTable } from "@/db/schema";
-import { cookies } from "next/headers";
-import { lucia } from "@/lib/auth";
+import {
+  createSession,
+  generateSessionToken,
+  setSessionTokenCookie,
+} from "@/lib/auth";
 import { eq } from "drizzle-orm";
 import { redirect } from "next/navigation";
 
 export async function login(values: { email: string; password: string }) {
   const { email, password } = values;
 
-  const [existingUser] = await db
-    .select()
-    .from(userTable)
-    .where(eq(userTable.email, email));
+  const existingUser = await db.query.userTable.findFirst({
+    where: eq(userTable.email, email),
+  });
+
   if (!existingUser) {
     throw new Error("Incorrect username or password");
   }
@@ -25,15 +28,15 @@ export async function login(values: { email: string; password: string }) {
     outputLen: 32,
     parallelism: 1,
   });
+
   if (!validPassword) {
     throw new Error("Incorrect username or password");
   }
-  const session = await lucia.createSession(existingUser.id, {});
-  const sessionCookie = lucia.createSessionCookie(session.id);
-  cookies().set(
-    sessionCookie.name,
-    sessionCookie.value,
-    sessionCookie.attributes,
-  );
+
+  const sessionToken = generateSessionToken();
+  const session = await createSession(sessionToken, existingUser.id);
+
+  await setSessionTokenCookie(sessionToken, session.expiresAt);
+
   return redirect("/admin");
 }
--- singh-sabha/lib/auth.ts
+++ singh-sabha/lib/auth.ts
@@ -1,65 +1,121 @@
-import { Lucia } from "lucia";
-import { DrizzlePostgreSQLAdapter } from "@lucia-auth/adapter-drizzle";
 import { userTable, sessionTable } from "@/db/schema";
 import { db } from "@/db/db";
 import { cookies } from "next/headers";
-import { cache } from "react";
+import { eq } from "drizzle-orm";
+import {
+  encodeBase32LowerCaseNoPadding,
+  encodeHexLowerCase,
+} from "@oslojs/encoding";
+import { sha256 } from "@oslojs/crypto/sha2";
 
-import type { Session, User } from "lucia";
+import type { User, Session } from "@/db/schema";
 
-const adapter = new DrizzlePostgreSQLAdapter(db, sessionTable, userTable);
+const SESSION_REFRESH_INTERVAL_MS = 1000 * 60 * 60 * 24 * 15;
+const SESSION_MAX_DURATION_MS = SESSION_REFRESH_INTERVAL_MS * 2;
+const SESSION_COOKIE_NAME = "session";
 
-export const lucia = new Lucia(adapter, {
-  sessionCookie: {
-    attributes: {
-      secure: process.env.NODE_ENV === "production",
-    },
-  },
-  getUserAttributes: (attributes) => {
-    return {
-      fullName: attributes.fullName,
-      email: attributes.email,
-      isAdmin: attributes.isAdmin,
-      isMod: attributes.isMod,
-    };
-  },
-});
+export function generateSessionToken(): string {
+  const bytes = new Uint8Array(20);
+  crypto.getRandomValues(bytes);
+  const token = encodeBase32LowerCaseNoPadding(bytes);
+  return token;
+}
 
-export const validateRequest = cache(
-  async (): Promise<
-    { user: User; session: Session } | { user: null; session: null }
-  > => {
-    const sessionId = cookies().get(lucia.sessionCookieName)?.value ?? null;
-    if (!sessionId) {
-      return {
-        user: null,
-        session: null,
-      };
-    }
+export async function createSession(
+  token: string,
+  userId: string,
+): Promise<Session> {
+  const sessionId = encodeHexLowerCase(sha256(new TextEncoder().encode(token)));
+  const session: Session = {
+    id: sessionId,
+    userId,
+    expiresAt: new Date(Date.now() + SESSION_MAX_DURATION_MS),
+  };
+  await db.insert(sessionTable).values(session);
+  return session;
+}
 
-    const result = await lucia.validateSession(sessionId);
-    // next.js throws when you attempt to set cookie when rendering page
-    try {
-      if (result.session && result.session.fresh) {
-        const sessionCookie = lucia.createSessionCookie(result.session.id);
-        cookies().set(
-          sessionCookie.name,
-          sessionCookie.value,
-          sessionCookie.attributes,
-        );
-      }
-      if (!result.session) {
-        const sessionCookie = lucia.createBlankSessionCookie();
-        cookies().set(
-          sessionCookie.name,
-          sessionCookie.value,
-          sessionCookie.attributes,
-        );
-      }
-    } catch {}
-    return result;
-  },
-);
+export async function validateSessionToken(
+  token: string,
+): Promise<SessionValidationResult> {
+  const sessionId = encodeHexLowerCase(sha256(new TextEncoder().encode(token)));
+  const sessionInDb = await db.query.sessionTable.findFirst({
+    where: eq(sessionTable.id, sessionId),
+  });
+  if (!sessionInDb) {
+    return { session: null, user: null };
+  }
+  if (Date.now() >= sessionInDb.expiresAt.getTime()) {
+    await db.delete(sessionTable).where(eq(sessionTable.id, sessionInDb.id));
+    return { session: null, user: null };
+  }
+  const user = await db.query.userTable.findFirst({
+    where: eq(userTable.id, sessionInDb.userId),
+  });
+
+  if (!user) {
+    await db.delete(sessionTable).where(eq(sessionTable.id, sessionInDb.id));
+    return { session: null, user: null };
+  }
+
+  if (
+    Date.now() >=
+    sessionInDb.expiresAt.getTime() - SESSION_REFRESH_INTERVAL_MS
+  ) {
+    sessionInDb.expiresAt = new Date(Date.now() + SESSION_MAX_DURATION_MS);
+    await db
+      .update(sessionTable)
+      .set({
+        expiresAt: sessionInDb.expiresAt,
+      })
+      .where(eq(sessionTable.id, sessionInDb.id));
+  }
+  return { session: sessionInDb, user };
+}
+
+export function deleteSessionTokenCookie(): void {
+  cookies().set(SESSION_COOKIE_NAME, "", {
+    httpOnly: true,
+    sameSite: "lax",
+    secure: process.env.NODE_ENV === "production",
+    maxAge: 0,
+    path: "/",
+  });
+}
+
+export async function invalidateSession(sessionId: string): Promise<void> {
+  await db.delete(sessionTable).where(eq(sessionTable.id, sessionId));
+}
+
+export async function invalidateUserSessions(userId: string): Promise<void> {
+  await db.delete(sessionTable).where(eq(userTable.id, userId));
+}
+
+export function getSessionToken(): string | undefined {
+  return cookies().get(SESSION_COOKIE_NAME)?.value;
+}
+
+export async function setSessionTokenCookie(
+  token: string,
+  expiresAt: Date,
+): Promise<void> {
+  const cookieStore = cookies();
+  cookieStore.set("session", token, {
+    httpOnly: true,
+    sameSite: "lax",
+    secure: process.env.NODE_ENV === "production",
+    expires: expiresAt,
+    path: "/",
+  });
+}
+
+export async function validateRequest(): Promise<SessionValidationResult> {
+  const sessionToken = getSessionToken();
+  if (!sessionToken) {
+    return { session: null, user: null };
+  }
+  return validateSessionToken(sessionToken);
+}
 
 export interface DatabaseUser {
   fullName: string;
@@ -68,9 +124,6 @@ export interface DatabaseUser {
   isMod: boolean;
 }
 
-declare module "lucia" {
-  interface Register {
-    Lucia: typeof lucia;
-    DatabaseUserAttributes: Omit<DatabaseUser, "id" | "passwordHash">;
-  }
-}
+export type SessionValidationResult =
+  | { session: Session; user: User }
+  | { session: null; user: null };
