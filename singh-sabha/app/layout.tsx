8a712b6ab0350bd003813b09f1e62492405c00a5
Bhavanvir Rai <bhavanvir.r@gmail.com>
2024-11-21 22:52:28 -0800

    Make it so Footer is on every page

--- singh-sabha/app/layout.tsx
+++ singh-sabha/app/layout.tsx
@@ -1,31 +1,31 @@
-import { Toaster } from "sonner";
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import { Toaster } from "sonner";
+import NavBar from "@/components/navbar";
+import { Footer } from "@/components/footer";
 
-const inter = Inter({
-  subsets: ["latin"],
-  display: "swap",
-});
+const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "Gurdwara Singh Sabha",
-  description:
-    "Website for the Singh Sabha Gurdwara in Victoria, British Columbia, Canada",
-  icons: {
-    icon: "/favicon.svg",
-  },
+  description: "Serving the Sikh community with devotion and compassion.",
 };
 
 export default function RootLayout({
   children,
-}: Readonly<{
+}: {
   children: React.ReactNode;
-}>) {
+}) {
   return (
-    <html lang="en" className={inter.className}>
-      <body>{children}</body>
-      <Toaster />
+    <html lang="en">
+      <body className={inter.className}>
+        <div className="flex flex-col min-h-screen">
+          <main className="flex-grow">{children}</main>
+          <Footer />
+        </div>
+        <Toaster />
+      </body>
     </html>
   );
 }
