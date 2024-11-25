7b0a4a6a9c6d7a07b636930dff9fd7bf03f61104
Bhavanvir Rai <bhavanvir.r@gmail.com>
2024-11-25 00:34:02 -0800

    Create helper endpoint for getting displayName for an event id

--- singh-sabha/lib/api/event-types/queries.ts
+++ singh-sabha/lib/api/event-types/queries.ts
@@ -5,6 +5,7 @@ import { db } from "@/db/db";
 import { eventTypeTable } from "@/db/schema";
 
 import type { EventType } from "@/db/schema";
+import { eq } from "drizzle-orm";
 
 export const GetAllEventTypes = cache(async (): Promise<EventType[]> => {
   try {
@@ -14,3 +15,15 @@ export const GetAllEventTypes = cache(async (): Promise<EventType[]> => {
     throw new Error(`Could not fetch event types: ${err}`);
   }
 });
+
+export const GetEventType = async ({ id }: { id: string }): Promise<string> => {
+  try {
+    const [eventType] = await db
+      .select()
+      .from(eventTypeTable)
+      .where(eq(eventTypeTable.id, id));
+    return eventType.displayName;
+  } catch (err) {
+    throw new Error(`Could not fetch event type: ${err}`);
+  }
+};
