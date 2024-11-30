bdf30103d334fe78470a8cb837a51b57573a6f6a
Bhavanvir Rai <bhavanvir.r@gmail.com>
2024-11-30 15:49:00 -0800

    Remove caching from all other queries in events

--- singh-sabha/lib/api/events/queries.ts
+++ singh-sabha/lib/api/events/queries.ts
@@ -1,55 +1,51 @@
 "use server";
-import { cache } from "react";
+
 import { db } from "@/db/db";
 import { eventTable, eventTypeTable } from "@/db/schema";
 import { eq, and, gte, lte } from "drizzle-orm";
 import type { EventWithType } from "@/db/schema";
 
-export const GetAllVerifiedEvents = cache(
-  async (): Promise<EventWithType[]> => {
-    try {
-      const events = await db
-        .select({
-          events: eventTable,
-          event_types: eventTypeTable,
-        })
-        .from(eventTable)
-        .leftJoin(eventTypeTable, eq(eventTable.type, eventTypeTable.id))
-        .where(eq(eventTable.verified, true));
+export const GetAllVerifiedEvents = async (): Promise<EventWithType[]> => {
+  try {
+    const events = await db
+      .select({
+        events: eventTable,
+        event_types: eventTypeTable,
+      })
+      .from(eventTable)
+      .leftJoin(eventTypeTable, eq(eventTable.type, eventTypeTable.id))
+      .where(eq(eventTable.verified, true));
 
-      return events.map(({ events, event_types }) => ({
-        ...events,
-        eventType: event_types ?? undefined,
-      }));
-    } catch (err) {
-      throw new Error(`Could not fetch events: ${err}`);
-    }
-  },
-);
+    return events.map(({ events, event_types }) => ({
+      ...events,
+      eventType: event_types ?? undefined,
+    }));
+  } catch (err) {
+    throw new Error(`Could not fetch events: ${err}`);
+  }
+};
 
-export const GetAllUnverifiedEvents = cache(
-  async (): Promise<EventWithType[]> => {
-    try {
-      const events = await db
-        .select({
-          events: eventTable,
-          event_types: eventTypeTable,
-        })
-        .from(eventTable)
-        .leftJoin(eventTypeTable, eq(eventTable.type, eventTypeTable.id))
-        .where(eq(eventTable.verified, false));
+export const GetAllUnverifiedEvents = async (): Promise<EventWithType[]> => {
+  try {
+    const events = await db
+      .select({
+        events: eventTable,
+        event_types: eventTypeTable,
+      })
+      .from(eventTable)
+      .leftJoin(eventTypeTable, eq(eventTable.type, eventTypeTable.id))
+      .where(eq(eventTable.verified, false));
 
-      return events.map(({ events, event_types }) => ({
-        ...events,
-        eventType: event_types ?? undefined,
-      }));
-    } catch (err) {
-      throw new Error(`Could not fetch events: ${err}`);
-    }
-  },
-);
+    return events.map(({ events, event_types }) => ({
+      ...events,
+      eventType: event_types ?? undefined,
+    }));
+  } catch (err) {
+    throw new Error(`Could not fetch events: ${err}`);
+  }
+};
 
-export const GetAllEvents = cache(async (): Promise<EventWithType[]> => {
+export const GetAllEvents = async (): Promise<EventWithType[]> => {
   try {
     const events = await db
       .select({
@@ -66,7 +62,7 @@ export const GetAllEvents = cache(async (): Promise<EventWithType[]> => {
   } catch (err) {
     throw new Error(`Could not fetch events: ${err}`);
   }
-});
+};
 
 export const GetEventsBetweenDates = async ({
   startDate,
