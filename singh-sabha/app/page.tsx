7043977d4ad680ffd1574f20557611aa5ba4ac51
Bhavanvir Rai <bhavanvir.r@gmail.com>
2024-11-30 15:14:56 -0800

    Add upcoming events dialog to home page and fetch upcoming events

--- singh-sabha/app/page.tsx
+++ singh-sabha/app/page.tsx
@@ -1,17 +1,32 @@
+import moment from "moment";
+
 import { GetActiveAnnouncement } from "@/lib/api/announcements/queries";
+import { GetEventsBetweenDates } from "@/lib/api/events/queries";
 import NavBar from "@/components/navbar";
 import { ActiveAnnouncement } from "@/components/announcement";
-import Hero from "@/components/hero";
+import HeroSection from "@/components/hero-section";
+import UpcomingEventsSection from "@/components/upcoming-events-section";
 
 export default async function Home() {
+  const currentDate = moment();
+  const weekStart = currentDate.startOf("week").toISOString();
+  const weekEnd = currentDate.endOf("week").toISOString();
+
   const [announcement] = await GetActiveAnnouncement();
+  const upcomingEvents = await GetEventsBetweenDates({
+    startDate: weekStart,
+    endDate: weekEnd,
+  });
 
   return (
     <>
       <NavBar currentLink="" />
       <ActiveAnnouncement announcement={announcement} />
       <main>
-        <Hero />
+        <HeroSection />
+        {upcomingEvents.length > 0 && (
+          <UpcomingEventsSection upcomingEvents={upcomingEvents} />
+        )}
       </main>
     </>
   );
