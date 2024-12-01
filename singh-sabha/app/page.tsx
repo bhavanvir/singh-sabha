029e049168d333045fbd1eca4f0fda1d2694f856
Bhavanvir Rai <bhavanvir.r@gmail.com>
2024-11-30 22:03:43 -0800

    Remove force-dynamic

--- singh-sabha/app/page.tsx
+++ singh-sabha/app/page.tsx
@@ -7,8 +7,6 @@ import { ActiveAnnouncement } from "@/components/announcement";
 import HeroSection from "@/components/hero-section";
 import UpcomingEventsSection from "@/components/upcoming-events-section";
 
-export const dynamic = "force-dynamic";
-
 export default async function Home() {
   const currentDate = moment();
   const weekStart = currentDate.startOf("week").toISOString();
