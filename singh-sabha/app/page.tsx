fadc4f3ef612d180ba5e293699f35e99840722a5
Bhavanvir Rai <bhavanvir.r@gmail.com>
2024-11-30 22:59:37 -0800

    Fix issue with upcoming events not showing due to moment time zone

--- singh-sabha/app/page.tsx
+++ singh-sabha/app/page.tsx
@@ -8,6 +8,8 @@ import HeroSection from "@/components/hero-section";
 import UpcomingEventsSection from "@/components/upcoming-events-section";
 
 export default async function Home() {
+  moment.locale("en-CA");
+
   const currentDate = moment();
   const weekStart = currentDate.startOf("week").toISOString();
   const weekEnd = currentDate.endOf("week").toISOString();
