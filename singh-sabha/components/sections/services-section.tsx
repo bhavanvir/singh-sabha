6e92c5c2ff0ace8a442679066ebea04b44d3781b
Bhavanvir Rai <bhavanvir.r@gmail.com>
2024-12-07 12:42:39 -0800

    Use a 3D perspective warping card

--- singh-sabha/components/sections/services-section.tsx
+++ singh-sabha/components/sections/services-section.tsx
@@ -1,13 +1,21 @@
 "use client";
-
 import * as React from "react";
-import { motion, useAnimation, useInView } from "framer-motion";
+import {
+  motion,
+  useAnimation,
+  useInView,
+  useSpring,
+  useTransform,
+} from "framer-motion";
 import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
-
 import { staggerContainer, fadeInWithDelay } from "./hero-section";
 import { EventType } from "@/db/schema";
-import { RainbowBorder } from "../animations/rainbow-border";
 import BookEventDialog from "../dialogs/book-event-dialog";
+
+// Interaction hyperparameters
+const sheenSize = 500;
+const cardRotation = 15;
+const cardScale = 1.07;
 
 interface ServicesSectionProps {
   eventTypes: EventType[];
@@ -46,28 +54,20 @@ export default function ServicesSection({ eventTypes }: ServicesSectionProps) {
               Explore the various services offered at Gurdwara Singh Sabha
             </p>
           </motion.div>
-
           <motion.div
             variants={staggerContainer}
-            className="flex flex-wrap justify-center gap-4"
+            className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4"
           >
             {eventTypes.map((type, index) => (
               <motion.div
                 key={type.id}
                 variants={fadeInWithDelay(0.3 + index * 0.1)}
-                className="w-full sm:w-[calc(50%-1rem)] md:w-[calc(33.333%-1rem)]"
+                className="flex"
               >
-                <RainbowBorder>
-                  <Card
-                    className="relative bg-background cursor-pointer"
-                    onClick={() => handleBookService(type)}
-                  >
-                    <CardHeader>
-                      <CardTitle>{type.displayName}</CardTitle>
-                    </CardHeader>
-                    <CardContent>{type.description}</CardContent>
-                  </Card>
-                </RainbowBorder>
+                <ServiceCard
+                  type={type}
+                  onBookService={() => handleBookService(type)}
+                />
               </motion.div>
             ))}
           </motion.div>
@@ -79,5 +79,113 @@ export default function ServicesSection({ eventTypes }: ServicesSectionProps) {
         eventTypes={selectedService}
       />
     </section>
+  );
+}
+
+function ServiceCard({
+  type,
+  onBookService,
+}: {
+  type: EventType;
+  onBookService: () => void;
+}) {
+  // Raw motion values
+  const xPcnt = useSpring(0, { bounce: 0 });
+  const yPcnt = useSpring(0, { bounce: 0 });
+  const mouseX = useSpring(0, { bounce: 0 });
+  const mouseY = useSpring(0, { bounce: 0 });
+  const scale = useSpring(1, { bounce: 0 });
+
+  // Calculated rotation values for styling
+  const rotateX = useTransform(
+    yPcnt,
+    [-0.5, 0.5],
+    [`-${cardRotation}deg`, `${cardRotation}deg`],
+  );
+  const rotateY = useTransform(
+    xPcnt,
+    [-0.5, 0.5],
+    [`${cardRotation}deg`, `-${cardRotation}deg`],
+  );
+
+  // Calculated sheen values for styling
+  const sheenX = useTransform(() => mouseX.get() - sheenSize / 2);
+  const sheenY = useTransform(() => mouseY.get() - sheenSize / 2);
+
+  // Helper function for getting mouse position
+  const getMousePosition = (e: React.MouseEvent<Element, MouseEvent>) => {
+    const { width, height, left, top } =
+      e.currentTarget.getBoundingClientRect();
+
+    const currentMouseX = e.clientX - left;
+    const currentMouseY = e.clientY - top;
+
+    return {
+      currentMouseX,
+      currentMouseY,
+      containerWidth: width,
+      containerHeight: height,
+    };
+  };
+
+  // Mouse event handlers
+  const handleMouseMove: React.MouseEventHandler = (e) => {
+    const { currentMouseX, currentMouseY, containerWidth, containerHeight } =
+      getMousePosition(e);
+
+    xPcnt.set(currentMouseX / containerWidth - 0.5);
+    yPcnt.set(currentMouseY / containerHeight - 0.5);
+
+    mouseX.set(currentMouseX);
+    mouseY.set(currentMouseY);
+  };
+
+  const handleMouseEnter: React.MouseEventHandler = (e) => {
+    const { currentMouseX, currentMouseY } = getMousePosition(e);
+
+    mouseX.jump(currentMouseX);
+    mouseY.jump(currentMouseY);
+    scale.set(cardScale);
+  };
+
+  const handleMouseLeave: React.MouseEventHandler = () => {
+    xPcnt.set(0);
+    yPcnt.set(0);
+    scale.set(1);
+  };
+
+  return (
+    <motion.div
+      onMouseEnter={handleMouseEnter}
+      onMouseMove={handleMouseMove}
+      onMouseLeave={handleMouseLeave}
+      onClick={onBookService}
+      className="w-full group"
+      style={{
+        transformStyle: "preserve-3d",
+        rotateX,
+        rotateY,
+        scale,
+      }}
+    >
+      <div className="relative bg-background rounded-xl border shadow-lg overflow-hidden min-h-[170px] cursor-pointer">
+        <motion.div
+          className="absolute z-10 opacity-0 group-hover:opacity-30 transition-opacity duration-200 rounded-full blur-md"
+          style={{
+            height: sheenSize,
+            width: sheenSize,
+            background: "radial-gradient(white, #3984ff00 80%)",
+            left: sheenX,
+            top: sheenY,
+          }}
+        />
+        <div className="p-4">
+          <h3 className="text-2xl font-semibold tracking-tight mb-2">
+            {type.displayName}
+          </h3>
+          <p className="text-muted-foreground md:text-lg">{type.description}</p>
+        </div>
+      </div>
+    </motion.div>
   );
 }
