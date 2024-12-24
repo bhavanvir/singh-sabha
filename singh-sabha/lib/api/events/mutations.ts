e5491c06ff8201bad5f5820ccf57e0e2e984ae9b
Bhavanvir Rai <bhavanvir.r@gmail.com>
2024-12-24 15:40:09 -0800

    Add new processDeposit api and update behaviour of CreateEvent

--- singh-sabha/lib/api/events/mutations.ts
+++ singh-sabha/lib/api/events/mutations.ts
@@ -2,8 +2,10 @@
 
 import { revalidatePath } from "next/cache";
 import { db } from "@/db/db";
-import { eventTable } from "@/db/schema";
+import { eventTable, eventTypeTable } from "@/db/schema";
 import { eq } from "drizzle-orm";
+
+import { sendEventEmails } from "@/lib/send-event-email";
 
 import type { Event } from "@/db/schema";
 
@@ -14,13 +16,23 @@ export const CreateEvent = async ({
     Event,
     "id" | "registrantFullName" | "registrantEmail" | "registrantPhoneNumber"
   >;
-}): Promise<void> => {
+}): Promise<string> => {
   try {
     const { ...eventData } = newEvent;
-    await db.insert(eventTable).values(eventData);
+
+    const [insertedEvent] = await db
+      .insert(eventTable)
+      .values(eventData)
+      .returning({ id: eventTable.id });
+
+    if (!insertedEvent) {
+      throw new Error("Event insertion failed, no ID returned");
+    }
 
     revalidatePath("/calendar");
     revalidatePath("/admin");
+
+    return insertedEvent.id;
   } catch (err) {
     throw new Error(`Could not add an event: ${err}`);
   }
@@ -59,5 +71,46 @@ export const DeleteEvent = async ({ id }: { id: string }): Promise<void> => {
     revalidatePath("/admin");
   } catch (err) {
     throw new Error(`Could not delete event: ${err}`);
+  }
+};
+
+export const processDeposit = async (id: string): Promise<void> => {
+  if (!id) {
+    throw new Error("Missing event ID");
+  }
+
+  try {
+    const event = await db
+      .select({
+        events: eventTable,
+        event_types: eventTypeTable,
+      })
+      .from(eventTable)
+      .leftJoin(eventTypeTable, eq(eventTable.type, eventTypeTable.id))
+      .where(eq(eventTable.id, id));
+
+    if (!event) {
+      throw new Error(`No event exists with ID ${id}`);
+    }
+
+    const [eventWithType] = event.map(({ events, event_types }) => ({
+      ...events,
+      eventType: event_types ?? undefined,
+    }));
+
+    if (!eventWithType.isDepositPaid) {
+      await UpdateEvent({
+        updatedEvent: {
+          ...eventWithType,
+          isDepositPaid: true,
+        },
+      });
+      sendEventEmails(
+        eventWithType,
+        `${process.env.NEXT_PUBLIC_BASE_URL}/api/send/confirmation`,
+      );
+    }
+  } catch (err) {
+    throw new Error(`Could not process deposit: ${err}`);
   }
 };
