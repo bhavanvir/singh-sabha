1723d142f44a5af886e8eabdf0249c8afee14f34
Bhavanvir Rai <bhavanvir.r@gmail.com>
2024-12-24 15:39:44 -0800

    Update book event dialog to route to Stripe checkout

--- singh-sabha/components/dialogs/book-event-dialog.tsx
+++ singh-sabha/components/dialogs/book-event-dialog.tsx
@@ -24,10 +24,11 @@ import { z } from "zod";
 import { toast } from "sonner";
 import { startOfDay, endOfDay } from "date-fns";
 
+import { Loader2 } from "lucide-react";
+
 import { ParametersForm } from "@/components/forms/parameters-form";
 import { userEventSchema } from "@/lib/event-schema";
 import { CreateEvent } from "@/lib/api/events/mutations";
-import { sendEventEmails } from "@/lib/send-event-email";
 
 import type { Event, EventType } from "@/db/schema";
 
@@ -42,6 +43,8 @@ export default function BookEventDialog({
   onClose,
   eventTypes,
 }: BookEventDialogProps) {
+  const [isLoading, setIsLoading] = React.useState<boolean>(false);
+
   const form = useForm<z.infer<typeof userEventSchema>>({
     resolver: zodResolver(userEventSchema),
     defaultValues: {
@@ -70,9 +73,35 @@ export default function BookEventDialog({
     onClose();
   };
 
+  const stripePayment = async (eventType: EventType, eventId: string) => {
+    try {
+      const stripeResponse = await fetch("/api/stripe/payment-intent", {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({
+          eventType,
+          eventId,
+        }),
+      });
+
+      if (!stripeResponse.ok) {
+        throw new Error("Failed to create Stripe session");
+      }
+
+      const { url } = await stripeResponse.json();
+      window.open(url, "_self");
+
+      return true;
+    } catch (error) {
+      return false;
+    }
+  };
+
   const handleSubmit: SubmitHandler<z.infer<typeof userEventSchema>> = (
     data,
   ) => {
+    setIsLoading(true);
+
     const startDateTime = startOfDay(data.dateRange.from);
     const endDateTime = endOfDay(data.dateRange.to || data.dateRange.from);
 
@@ -89,23 +118,25 @@ export default function BookEventDialog({
       isVerified: false,
       frequencyRule: null,
       isPublic: data.isPublic,
+      isDepositPaid: false,
+      isBalancePaid: false,
     };
+
+    const eventType = eventTypes.find((type) => type.id === data.type);
 
     toast.promise(
       async () => {
-        const createdEvent = await CreateEvent({ newEvent });
-        await sendEventEmails(newEvent, "/api/send/confirmation");
-        return createdEvent;
+        const eventId = await CreateEvent({ newEvent });
+        const paymentSuccess = await stripePayment(eventType!, eventId);
+        setIsLoading(false);
+        return paymentSuccess;
       },
       {
-        loading: "Submitting event and sending confirmation...",
-        success:
-          "Event booking submitted and confirmation email sent successfully!",
-        error: "Failed to submit event booking or send confirmation email",
+        loading: "Submitting event...",
+        success: "Event booking submitted successfully!",
+        error: "Failed to submit event booking",
       },
     );
-
-    handleClose();
   };
 
   return (
@@ -187,7 +218,31 @@ export default function BookEventDialog({
             </div>
 
             <DialogFooter>
-              <Button type="submit">Submit</Button>
+              <Button
+                type="submit"
+                disabled={isLoading}
+                className="flex items-center"
+              >
+                {isLoading ? (
+                  <>
+                    <Loader2 className="h-4 w-4 animate-spin" />
+                    Please wait
+                  </>
+                ) : (
+                  <>
+                    <svg
+                      role="img"
+                      viewBox="0 0 24 24"
+                      xmlns="http://www.w3.org/2000/svg"
+                      className="h-5 w-5 fill-[#635BFF]"
+                    >
+                      <title>Stripe</title>
+                      <path d="M13.976 9.15c-2.172-.806-3.356-1.426-3.356-2.409 0-.831.683-1.305 1.901-1.305 2.227 0 4.515.858 6.09 1.631l.89-5.494C18.252.975 15.697 0 12.165 0 9.667 0 7.589.654 6.104 1.872 4.56 3.147 3.757 4.992 3.757 7.218c0 4.039 2.467 5.76 6.476 7.219 2.585.92 3.445 1.574 3.445 2.583 0 .98-.84 1.545-2.354 1.545-1.875 0-4.965-.921-6.99-2.109l-.9 5.555C5.175 22.99 8.385 24 11.714 24c2.641 0 4.843-.624 6.328-1.813 1.664-1.305 2.525-3.236 2.525-5.732 0-4.128-2.524-5.851-6.594-7.305h.003z" />
+                    </svg>
+                    Proceed with Payment
+                  </>
+                )}
+              </Button>
             </DialogFooter>
           </form>
         </Form>
