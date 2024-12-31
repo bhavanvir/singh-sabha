647d78ce63e47323a9112d612124c7e1c543fc7c
Bhavanvir Rai <bhavanvir.r@gmail.com>
2024-12-30 16:49:22 -0800

    Update navbar

--- singh-sabha/components/navbar.tsx
+++ singh-sabha/components/navbar.tsx
@@ -1,17 +1,24 @@
+"use client";
+
 import Link from "next/link";
+import { usePathname } from "next/navigation";
 import { Button } from "@/components/ui/button";
 import { Drawer, DrawerContent, DrawerTrigger } from "@/components/ui/drawer";
 import { Menu, FlagTriangleLeft } from "lucide-react";
 
-interface NavBarProps {
-  currentLink: string;
-}
+export default function NavBar() {
+  const pathname = usePathname();
 
-export default function NavBar({ currentLink }: NavBarProps) {
   const links = [
     { href: "/calendar", label: "Calendar" },
     { href: "/hukamnama", label: "Hukamnama" },
   ];
+
+  const isCurrentPath = (linkHref: string) => {
+    const path = pathname.split("/")[1];
+    const linkPath = linkHref.split("/")[1];
+    return path === linkPath;
+  };
 
   return (
     <header className="z-10 sticky top-0 flex h-16 items-center gap-4 px-4 md:px-6 border-b border-border/40 bg-background/95 backdrop-blur supports-[backdrop-filter]:bg-background/60 dark:border-border">
@@ -29,7 +36,7 @@ export default function NavBar({ currentLink }: NavBarProps) {
             key={link.href}
             href={link.href}
             className={`transition-colors hover:text-primary ${
-              currentLink.toLowerCase() === link.label.toLowerCase()
+              isCurrentPath(link.href)
                 ? "text-primary"
                 : "text-muted-foreground"
             }`}
@@ -57,7 +64,7 @@ export default function NavBar({ currentLink }: NavBarProps) {
                 key={link.href}
                 href={link.href}
                 className={`transition-colors hover:text-primary ${
-                  currentLink.toLowerCase() === link.label.toLowerCase()
+                  isCurrentPath(link.href)
                     ? "text-primary"
                     : "text-muted-foreground"
                 }`}
