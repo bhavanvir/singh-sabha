d68f5add6d8037bcd60fd5d5336575502426724c
Bhavanvir Rai <bhavanvir.r@gmail.com>
2024-12-30 19:32:28 -0800

    Resize YouTube embed and add hover colour change

--- singh-sabha/components/sections/youtube-live-section.tsx
+++ singh-sabha/components/sections/youtube-live-section.tsx
@@ -5,6 +5,7 @@ import { motion, useAnimation, useInView } from "framer-motion";
 import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
 
 import { staggerContainer, fadeInWithDelay } from "./hero-section";
+import Link from "next/link";
 
 export function YoutubeLiveSection() {
   const ref = React.useRef(null);
@@ -37,19 +38,24 @@ export function YoutubeLiveSection() {
           </motion.div>
 
           <motion.div variants={fadeInWithDelay(0.3)}>
-            <Card className="mx-auto overflow-hidden max-w-[900px]">
+            <Card className="mx-auto overflow-hidden">
               <CardHeader>
-                <CardTitle className="flex items-center justify-center space-x-2">
-                  <svg
-                    role="img"
-                    viewBox="0 0 24 24"
-                    xmlns="http://www.w3.org/2000/svg"
-                    className="main-grid-item-icon h-6 w-6"
+                <CardTitle className="flex items-center justify-center">
+                  <Link
+                    href="https://www.youtube.com/@GurdwaraSinghSabhaVictoria"
+                    className="flex items-center space-x-2 hover:text-muted-foreground hover:fill-muted-foreground transition-colors"
                   >
-                    <title>YouTube</title>
-                    <path d="M23.498 6.186a3.016 3.016 0 0 0-2.122-2.136C19.505 3.545 12 3.545 12 3.545s-7.505 0-9.377.505A3.017 3.017 0 0 0 .502 6.186C0 8.07 0 12 0 12s0 3.93.502 5.814a3.016 3.016 0 0 0 2.122 2.136c1.871.505 9.376.505 9.376.505s7.505 0 9.377-.505a3.015 3.015 0 0 0 2.122-2.136C24 15.93 24 12 24 12s0-3.93-.502-5.814zM9.545 15.568V8.432L15.818 12l-6.273 3.568z" />
-                  </svg>
-                  <span>Gurdwara Singh Sabha (Victoria) Live</span>
+                    <svg
+                      role="img"
+                      viewBox="0 0 24 24"
+                      xmlns="http://www.w3.org/2000/svg"
+                      className="main-grid-item-icon h-6 w-6 "
+                    >
+                      <title>YouTube</title>
+                      <path d="M23.498 6.186a3.016 3.016 0 0 0-2.122-2.136C19.505 3.545 12 3.545 12 3.545s-7.505 0-9.377.505A3.017 3.017 0 0 0 .502 6.186C0 8.07 0 12 0 12s0 3.93.502 5.814a3.016 3.016 0 0 0 2.122 2.136c1.871.505 9.376.505 9.376.505s7.505 0 9.377-.505a3.015 3.015 0 0 0 2.122-2.136C24 15.93 24 12 24 12s0-3.93-.502-5.814zM9.545 15.568V8.432L15.818 12l-6.273 3.568z" />
+                    </svg>
+                    <span>Gurdwara Singh Sabha (Victoria) Live</span>
+                  </Link>
                 </CardTitle>
               </CardHeader>
               <CardContent className="p-0 aspect-video">
