76ca6c070246b4840957b8ab35abaa8743717c15
Bhavanvir Rai <bhavanvir.r@gmail.com>
2025-01-03 16:46:40 -0800

    Add language switcher

--- singh-sabha/components/daily-hukamnama.tsx
+++ singh-sabha/components/daily-hukamnama.tsx
@@ -1,10 +1,12 @@
 "use client";
 
 import * as React from "react";
-import { Book, Pen, Loader2, Music2 } from "lucide-react";
 import { Separator } from "@/components/ui/separator";
 import { ScrollArea } from "@/components/ui/scroll-area";
 import { Badge } from "@/components/ui/badge";
+import { Button } from "@/components/ui/button";
+
+import { Book, Pen, Loader2, Music2, Languages } from "lucide-react";
 
 import { notoSansGurmukhi } from "@/app/fonts";
 import { cn } from "@/lib/utils";
@@ -15,6 +17,7 @@ export default function DailyHukamnama() {
   const [hukamnama, setHukamnama] = React.useState<HukamnamaRoot | null>(null);
   const [isLoading, setIsLoading] = React.useState(true);
   const [error, setError] = React.useState<string | null>(null);
+  const [transLang, setTransLang] = React.useState<"eng" | "span">("eng");
 
   React.useEffect(() => {
     const fetchHukamnama = async () => {
@@ -35,6 +38,10 @@ export default function DailyHukamnama() {
     fetchHukamnama();
   }, []);
 
+  const toggleLanguage = () => {
+    setTransLang(transLang === "eng" ? "span" : "eng");
+  };
+
   return (
     <div className="mx-auto container py-4 px-4 md:px-6">
       {isLoading && (
@@ -44,23 +51,30 @@ export default function DailyHukamnama() {
       )}
       {hukamnama ? (
         <>
-          <div className="flex flex-col sm:flex-row items-center space-y-2 sm:space-y-0 sm:space-x-2">
-            <h1 className="text-xl sm:text-2xl font-bold">
-              Today&apos;s Hukamnama
-            </h1>
-            <div className="flex space-x-2">
-              <Badge>
-                {hukamnama.date.gregorian.month} {hukamnama.date.gregorian.date}
-                {", "}
-                {hukamnama.date.gregorian.year}
-              </Badge>
-              <Badge>
-                {hukamnama.date.nanakshahi.punjabi.month}{" "}
-                {hukamnama.date.nanakshahi.punjabi.date}
-                {", "}
-                {hukamnama.date.nanakshahi.punjabi.year}
-              </Badge>
+          <div className="flex flex-col sm:flex-row items-center justify-between space-y-2 sm:space-y-0 sm:space-x-2">
+            <div className="flex flex-col sm:flex-row items-center space-y-2 sm:space-y-0 sm:space-x-2">
+              <h1 className="text-xl sm:text-2xl font-bold">
+                Today&apos;s Hukamnama
+              </h1>
+              <div className="flex space-x-2">
+                <Badge>
+                  {hukamnama.date.gregorian.month}{" "}
+                  {hukamnama.date.gregorian.date}
+                  {", "}
+                  {hukamnama.date.gregorian.year}
+                </Badge>
+                <Badge>
+                  {hukamnama.date.nanakshahi.punjabi.month}{" "}
+                  {hukamnama.date.nanakshahi.punjabi.date}
+                  {", "}
+                  {hukamnama.date.nanakshahi.punjabi.year}
+                </Badge>
+              </div>
             </div>
+            <Button onClick={toggleLanguage} variant="outline" size="sm">
+              <Languages className="h-4 w-4" />
+              {transLang === "eng" ? "Español" : "English"}
+            </Button>
           </div>
 
           <div className="flex flex-col sm:flex-row items-center text-muted-foreground text-sm md:text-md mt-2 sm:mt-0">
@@ -90,7 +104,7 @@ export default function DailyHukamnama() {
             </span>
           </div>
 
-          <ScrollArea className="h-[calc(100vh-16.5rem)] sm:h-[calc(100vh-11rem)] mt-4 rounded-md border p-4 mb-4">
+          <ScrollArea className="h-[calc(100vh-18.25rem)] sm:h-[calc(100vh-11rem)] mt-4 rounded-md border p-4 mb-4">
             <section className="grid grid-cols-1 gap-4">
               {hukamnama.hukamnama.map((item, index) => (
                 <div key={index}>
@@ -101,7 +115,9 @@ export default function DailyHukamnama() {
                     {item.line.transliteration.english.text}
                   </p>
                   <p className="text-md text-muted-foreground">
-                    {item.line.translation.english.default}
+                    {transLang === "eng"
+                      ? item.line.translation.english.default
+                      : item.line.translation.spanish}
                   </p>
                 </div>
               ))}
@@ -110,7 +126,11 @@ export default function DailyHukamnama() {
         </>
       ) : (
         <div className="h-[calc(100vh-6rem)] flex items-center justify-center text-center">
-          <p>No Hukamnama data available.</p>
+          <p>
+            {transLang === "eng"
+              ? "No Hukamnama data available."
+              : "No hay datos de Hukamnama disponibles."}
+          </p>
           {error && <p>{error}</p>}
         </div>
       )}
