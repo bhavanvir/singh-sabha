81196746cfe24a1d22fc6659757f98a112cb92e0
Bhavanvir Rai <bhavanvir.r@gmail.com>
2025-01-14 23:18:10 -0800

    Revalidate YouTube cache every 30 mins

--- singh-sabha/app/api/youtube/route.ts
+++ singh-sabha/app/api/youtube/route.ts
@@ -7,7 +7,7 @@ export async function GET() {
   try {
     const response = await fetch(
       `https://www.googleapis.com/youtube/v3/search?part=snippet&channelId=${CHANNEL_ID}&eventType=live&type=video&key=${YOUTUBE_API_KEY}`,
-      { next: { revalidate: 1 * 60 * 60 } },
+      { next: { revalidate: 0.5 * 60 * 60 } }, // Every 30 minutes
     );
 
     if (!response.ok) {
