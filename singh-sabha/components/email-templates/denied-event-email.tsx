e88d43e41faa7b0894485624eb208b101c5071e9
Bhavanvir Rai <bhavanvir.r@gmail.com>
2025-01-14 16:55:05 -0800

    Denied event emails should not have a time

--- singh-sabha/components/email-templates/denied-event-email.tsx
+++ singh-sabha/components/email-templates/denied-event-email.tsx
@@ -52,12 +52,6 @@ export default function DeniedEventEmail({
                   format(event.end, "MMMM d, yyyy") &&
                   ` - ${format(event.end, "MMMM d, yyyy")}`}
               </Text>
-              <Text className="text-gray-700 text-sm">
-                <span className="font-medium">Time:</span>{" "}
-                {event.allDay
-                  ? "All Day"
-                  : `${format(event.start, "h:mm a")} - ${format(event.end, "h:mm a")}`}
-              </Text>
             </div>
             {denialReason && (
               <div className="bg-red-50 rounded-md border-l-4 border-red-400 p-4 mb-6">
