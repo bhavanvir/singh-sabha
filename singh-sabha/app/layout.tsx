65a3adc529d9fc300e8c2eefce48014f3f3ab41e
Bhavanvir Rai <bhavanvir.r@gmail.com>
2025-01-24 17:48:24 -0800

    /favicon -> /assets/favicon

--- singh-sabha/app/layout.tsx
+++ singh-sabha/app/layout.tsx
@@ -36,17 +36,21 @@ export default function RootLayout({
         <link
           rel="icon"
           type="image/png"
-          href="/favicon/favicon-96x96.png"
+          href="/assets/favicon/favicon-96x96.png"
           sizes="96x96"
         />
-        <link rel="icon" type="image/svg+xml" href="/favicon/favicon.svg" />
-        <link rel="shortcut icon" href="/favicon/favicon.ico" />
+        <link
+          rel="icon"
+          type="image/svg+xml"
+          href="/assets/favicon/favicon.svg"
+        />
+        <link rel="shortcut icon" href="/assets/favicon/favicon.ico" />
         <link
           rel="apple-touch-icon"
           sizes="180x180"
-          href="/favicon/apple-touch-icon.png"
+          href="/assets/favicon/apple-touch-icon.png"
         />
-        <link rel="manifest" href="/favicon/site.webmanifest" />
+        <link rel="manifest" href="/assets/favicon/site.webmanifest" />
       </head>
       <body className={`${inter.className}`}>
         <div className="flex flex-col min-h-screen">
